866edd3141666b6c664eebfff1cab674f601c008
RajPatil21 <102685436+RajPatil21@users.noreply.github.com>
2025-02-05 23:08:47 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 int main() {
     Graph g;
 
-    // Adding edges
-    g.addEdge(1, 2, 10);
-    g.addEdge(1, 3, 15);
-    g.addEdge(2, 4, 12);
-    g.addEdge(3, 5, 10);
-    g.addEdge(4, 5, 2);
+    // Adding edges with different weights
+    g.addEdge(1, 2, 7);
+    g.addEdge(1, 3, 9);
+    g.addEdge(1, 6, 14);
+    g.addEdge(2, 3, 10);
+    g.addEdge(2, 4, 15);
+    g.addEdge(3, 4, 11);
+    g.addEdge(3, 6, 2);
+    g.addEdge(4, 5, 6);
+    g.addEdge(5, 6, 9);
 
     cout << "Graph Representation:" << endl;
     g.displayGraph();
